5e307b21e4799a87eef714232527e82cf941bb31
Hubert Rytel <h.rytel@gmail.com>
2023-04-17 11:30:28 +0200

    Initial commit

---
+++ Pods/Target Support Files/Pods-SimpleRedditReader-SimpleRedditReaderUITests/Pods-SimpleRedditReader-SimpleRedditReaderUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SimpleRedditReader_SimpleRedditReaderUITests : NSObject
+@end
+@implementation PodsDummy_Pods_SimpleRedditReader_SimpleRedditReaderUITests
+@end
